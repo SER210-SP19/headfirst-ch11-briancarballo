68e2a05ea096701fa5cb5eb8494e900aa091d2c3
Brian Carballo <brian.carballo@quinnipiac.edu>
2019-03-21 21:24:47 -0400

    Final Demo

--- src/main/java/edu/quinnipiac/ser210/workout/MainActivity.java
+++ src/main/java/edu/quinnipiac/ser210/workout/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.quinnipiac.ser210.workout;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,8 +21,19 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
-        startActivity(intent);
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null) {
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+            startActivity(intent);
+        }
     }
 }
--- src/main/java/edu/quinnipiac/ser210/workout/WorkoutDetailFragment.java
+++ src/main/java/edu/quinnipiac/ser210/workout/WorkoutDetailFragment.java
@@ -2,6 +2,8 @@ package edu.quinnipiac.ser210.workout;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,18 @@ public class WorkoutDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null){
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong("workoutId", workoutId);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
